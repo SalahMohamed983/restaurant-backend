390495e7fe703343eb6e74e011b6e8c4c28e56c6
Salah Eldein Mohamed <sm8109256@gmail.com>
2026-01-20 08:14:10 +0200

    Update IPermissionService.cs

--- ResturantBusinessLayer/Services/Interfaces/IPermissionService.cs
+++ ResturantBusinessLayer/Services/Interfaces/IPermissionService.cs
@@ -8,9 +8,7 @@ namespace ResturantBusinessLayer.Services.Interfaces
     public interface IPermissionService
     {
         Task<IEnumerable<PermissionDto>> GetAllAsync();
-        Task<int> CreateAsync(PermissionDto dto);
-        Task UpdateAsync(PermissionDto dto);
-        Task DeleteAsync(int id);
+    
         Task<bool> ExistsAsync(string code);
     }
 }
